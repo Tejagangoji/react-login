3200b492a75eec7d303b6fc57117532dadedf283
Tejagangoji <124460877+Tejagangoji@users.noreply.github.com>
2023-02-21 16:47:02 +0530

    update port

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ app.post('/token', async (req, res) => {
 })
 
 
-app.listen(5000, () => console.log("server is running"));
+app.listen(process.env.PORT || 5000, () => console.log("server is running"));
